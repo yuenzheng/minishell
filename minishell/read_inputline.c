4c70faa73a4940e12ece20642273cd132821c8fd
ychng <ychng@student.42kl.edu.my>
2024-03-27 06:50:20 +0800

    remove a free line in has nio erro because redundant

--- minishell/read_inputline.c
+++ minishell/read_inputline.c
@@ -6,7 +6,7 @@
 /*   By: ychng <ychng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 19:39:08 by ychng             #+#    #+#             */
-/*   Updated: 2024/03/27 05:58:09 by ychng            ###   ########.fr       */
+/*   Updated: 2024/03/27 06:49:45 by ychng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,6 @@ bool	has_no_error(char *input)
 		free(token);
 		token = get_next_token(NULL, false);
 	}
-	free(token);
 	return (true);
 }
 
